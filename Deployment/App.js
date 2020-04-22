21892eb55f21856bfff6886c9ecc97933743b91b
javier_soler <javier.soler.jsa@gmail.com>
2020-04-22 22:44:30 +0200

    Swipe items importado

--- Deployment/App.js
+++ Deployment/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Main from './screens/Main';
+import SwipeButtonCustom from './components/SwipeButtonCustom';
 
 export default function App() {
   return (
-      <Main />
+      // <Main />
+      <SwipeButtonCustom/>
   );
 }
